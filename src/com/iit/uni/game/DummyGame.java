a321a1d90501f41ca1eb2a77c48a5ae7799ee28c
rendzso <bodnarreno@gmail.com>
2018-04-21 20:01:37 +0200

    bounding proba. valós adatokat használ már, Scalelésnél nem jó a box utána tolás, ugrást megcsinálni, és gravitációt is.

--- src/com/iit/uni/game/DummyGame.java
+++ src/com/iit/uni/game/DummyGame.java
@@ -339,9 +339,11 @@ public class DummyGame implements IGameLogic {
 	@Override
 	public void update(float interval) {
 
+		//System.out.println(gameItem.mBBoxTransformed.CheckOverlapping(AllItems.get(1).GetCurrentBBox()));
+
 		//gameItem.SetBoundingBox();
-		System.out.println("x: " + gameItem.GetX() + " y: " + gameItem.GetY() + " speed: " +speedY);
-		gameItem.DrawBoundingBox();
+		//System.out.println("x: " + gameItem.GetX() + " y: " + gameItem.GetY() + " speed: " +speedY);
+		//gameItem.DrawBoundingBox();
 
 
 		if(up == 1) {
